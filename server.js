19db81a89022d3bffefe86fb47ab20acd260a707
YuSCJ <jackyu910335@gmail.com>
2025-12-07 23:41:51 +0800

    改成新北市

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ const getKaohsiungWeather = async (req, res) => {
       {
         params: {
           Authorization: CWA_API_KEY,
-          locationName: "臺南市",
+          locationName: "新北市",
         },
       }
     );
